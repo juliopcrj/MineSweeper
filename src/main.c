6c304fb5bc3c652cc296d3c8403655384bd512c4
Julio Pinto Coelho <juliopcrj@gmail.com>
2019-08-20 17:38:02 -0300

    Adding a few colors

--- src/main.c
+++ src/main.c
@@ -3,8 +3,6 @@
 #include <time.h>
 #include <mines.h>
 
-#define clear() printf("\033[H\033[J")
-
 int main(int argc, char **argv){
 	// seeding the rand
 	srand(time(NULL));
@@ -15,15 +13,17 @@ int main(int argc, char **argv){
 
 	int i,j;
 	do{
-		clear();
+		system(CLEAR);
 		plot_table(t);
 		printf("Please pick your line and column\nType 0 0 to end\n");
 		scanf("%d %d", &i, &j);
 		if(i == 0){
 			j = check_victory(t);
 			if(j == 1){
+				printf("\33[0;32m");
 				printf("VICTORY!\n");
 			}else{
+				printf("\33[0;31m");
 				printf("TOTAL DEFEAT!\n");
 			}
 			return 0;
